265c065cda149b6e28e03968673426aa1650be09
Tim Phillips <tim.p.phillips@gmail.com>
2019-06-15 12:17:28 -0700

    now

---
+++ server.js
@@ -0,0 +1,14 @@
+const { send } = require('micro')
+const { readFile } = require('fs')
+const { promisify } = require('util')
+const { join } = require('path')
+const readFilePromise = promisify(readFile)
+
+module.exports = async (req, res) => {
+  console.log(req.url)
+  console.log(req.headers)
+  res.setHeader('Cache-Control', 's-maxage=0')
+  res.setHeader('Content-Type', 'application/liquid')
+  const read = await readFilePromise(join(__dirname, 'build', 'index.html'))
+  return send(res, 200, read.toString())
+}
